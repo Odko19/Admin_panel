d16364140ddf9c9e0b388797d3cbf1145f56d93e
Odko19 <gerelrosegerel@gmail.com>
2023-09-21 10:54:59 +0800

    ADD ebarimt

--- src/componants/EbarimtId.js
+++ src/componants/EbarimtId.js
@@ -45,7 +45,7 @@ function EbarimtId() {
   const onSearch = (value, page, limit) => {
     page = 1;
     limit = 7;
-    let url = `${process.env.REACT_APP_BASE_URL}/order`;
+    let url = `${process.env.REACT_APP_BASE_URL}/ebarimt`;
     const queryParams = [];
 
     if (value) {
@@ -68,25 +68,15 @@ function EbarimtId() {
       .then((result) => {
         setData(
           result.data.map((row, i) => ({
-            FIRST_NAME: row.FIRST_NAME,
-            LAST_NAME: row.LAST_NAME,
-            CUST_TYPE: row.CUST_TYPE,
-            MOBILE: row.MOBILE,
-            EMAIL: row.EMAIL,
-            CITY: row.CITY,
-            DISTRICT: row.DISTRICT,
-            KHOROO: row.KHOROO,
-            APARTMENT: row.APARTMENT,
-            DOOR: row.DOOR,
-            ENTRANCE: row.ENTRANCE,
-            REGISTER: row.REGISTER,
-            RESULT: row.RESULT,
-            SERVICE: row.SERVICE,
             ID: row.ID,
-            OPERATOR_ID: row.OPERATOR_ID,
-            OPERATOR_STATUS: row.OPERATOR_STATUS,
-            ADDITIONAL: row.ADDITIONAL,
+            CUST_ID: row.CUST_ID,
+            REGNO: row.REGNO,
+            EBARIMT_ID: row.EBARIMT_ID,
             CREATED_AT: moment(row.CREATED_AT).format("L"),
+            UPDATED_AT: row.UPDATED_AT,
+            ID_CHECK: row.ID_CHECK,
+            STAFF_ID: row.STAFF_ID,
+            MOBILE: row.MOBILE,
             key: i,
           }))
         );
@@ -107,10 +97,10 @@ function EbarimtId() {
     myHeaders.append("Content-Type", "application/json");
     var raw = JSON.stringify({
       ID: record,
-      OPERATOR_ID: user.firstName,
-      OPERATOR_STATUS: 1,
+      STAFF_ID: user.firstName,
+      ID_CHECK: 1,
     });
-
+    console.log(raw);
     var requestOptions = {
       method: "PUT",
       headers: myHeaders,
@@ -118,7 +108,7 @@ function EbarimtId() {
       redirect: "follow",
     };
 
-    fetch(`${process.env.REACT_APP_BASE_URL}/order`, requestOptions)
+    fetch(`${process.env.REACT_APP_BASE_URL}/ebarimt`, requestOptions)
       .then((response) => response.json())
       .then((result) => {
         if (result.success === true) {
@@ -162,7 +152,6 @@ function EbarimtId() {
 
   // Pagination
   function handlePageChange(page) {
-    console.log(page);
     if (url) {
       const jsonObject = {};
 
@@ -170,7 +159,6 @@ function EbarimtId() {
         const [key, value] = item.split("=");
         jsonObject[key] = value;
       }
-      console.log(url);
       jsonObject.page = page;
       const queryString = Object.entries(jsonObject)
         .map(([key, value]) => `${key}=${value}`)
@@ -222,45 +210,74 @@ function EbarimtId() {
 
   const columns = [
     {
-      title: "ID",
+      title: "№",
       dataIndex: "ID",
       key: "ID",
     },
     {
-      title: "CUST_ID",
+      title: "Гэрээний дугаар",
       dataIndex: "CUST_ID",
       key: "CUST_ID",
     },
     {
-      title: "REGNO",
+      title: "Регистерийн дугаар",
       dataIndex: "REGNO",
       key: "REGNO",
     },
     {
-      title: "EBARIMT_ID",
+      title: "Ибаримтын дугаар",
       dataIndex: "EBARIMT_ID",
       key: "EBARIMT_ID",
     },
 
     {
-      title: "CREATED_AT",
+      title: "Огноо",
       dataIndex: "CREATED_AT",
       key: "CREATED_AT",
     },
     {
-      title: "MOBILE",
+      title: "Утасны дугаар",
       dataIndex: "MOBILE",
       key: "MOBILE",
     },
 
     {
-      title: "Дэлгэрэнгүй",
+      title: "Баталгаажуулалт",
       width: "12%",
       dataIndex: "",
       key: "action",
       render: (record) => (
-        <Button type="primary" onClick={() => showModal(record)}>
-          Харах
+        <Button
+          onClick={() => showModal(record)}
+          type="primary"
+          style={{ border: "none" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={
+                record.ID_CHECK === 0
+                  ? {
+                      backgroundColor: "red",
+                      width: "10px",
+                      height: "10px",
+                      marginRight: "10px",
+                    }
+                  : {
+                      backgroundColor: "#00b96b",
+                      width: "10px",
+                      height: "10px",
+                      marginRight: "10px",
+                    }
+              }
+            ></div>
+            <span> Баталгаажуулах</span>
+          </div>
         </Button>
       ),
     },
@@ -282,96 +299,45 @@ function EbarimtId() {
       >
         <div className="pt">
           <div className="p1">
-            <div style={{ width: "160px" }}>Овог : </div>
-            {modaldata.LAST_NAME}
+            <div style={{ width: "160px" }}>ID : </div>
+            {modaldata.ID}
           </div>
           <div className="p2">
-            <div style={{ width: "160px" }}>Нэр : </div>
-            {modaldata.FIRST_NAME}
+            <div style={{ width: "160px" }}>CREATED_AT : </div>
+            {modaldata.CREATED_AT}
           </div>
           <div className="p1">
-            <div style={{ width: "160px" }}>Утас : </div>
+            <div style={{ width: "160px" }}>CUST_ID : </div>
+            {modaldata.CUST_ID}
+          </div>
+          <div className="p2">
+            <div style={{ width: "160px" }}>EBARIMT_ID : </div>
+            {modaldata.EBARIMT_ID}
+          </div>
+
+          <div className="p1">
+            <div style={{ width: "160px" }}>MOBILE: </div>
             {modaldata.MOBILE}
           </div>
           <div className="p2">
-            <div style={{ width: "160px" }}>Регистр : </div>
-            {modaldata.REGISTER}
+            <div style={{ width: "160px" }}>REGNO : </div>
+            {modaldata.REGNO}
           </div>
 
-          <div className="p1">
-            <div style={{ width: "160px" }}>Төрөл</div>
-            {modaldata.CUST_TYPE === "PSN"
-              ? "Хувь хүн"
-              : modaldata.CUST_TYPE === "GOV"
-              ? "Байгууллага"
-              : "Бизнес"}
-          </div>
-          {modaldata.CUST_TYPE === "GOV" || modaldata.CUST_TYPE === "BUS" ? (
-            <div className="p2">
-              <div style={{ width: "160px" }}>Байгууллагын нэр : </div>
-              {modaldata.CUST_NAME}
-            </div>
-          ) : (
-            ""
-          )}
-          {modaldata.CUST_TYPE === "GOV" || modaldata.CUST_TYPE === "BUS" ? (
-            <div className="p2">
-              <div style={{ width: "160px" }}>Байгууллагын Регистр : </div>
-              {modaldata.TAX_ID}
-            </div>
-          ) : (
-            ""
-          )}
-          <div className="p1">
-            <div style={{ width: "160px" }}>Хот/Аймаг: </div>
-            {modaldata.CITY}
-          </div>
-          <div className="p2">
-            <div style={{ width: "160px" }}>Дүүрэг / Сум : </div>
-            {modaldata.DISTRICT}
-          </div>
-          <div className="p1">
-            <div style={{ width: "160px" }}>Хороо : </div>
-            {modaldata.KHOROO}
-          </div>
-          <div className="p2">
-            <div style={{ width: "160px" }}>Байр : </div>
-            {modaldata.APARTMENT}
-          </div>
-          <div className="p1">
-            <div style={{ width: "160px" }}>Орц : </div>
-            {modaldata.ENTRACE}
-          </div>
-          <div className="p2">
-            <div style={{ width: "160px" }}>Тоот : </div>
-            {modaldata.DOOR}
-          </div>
-
-          <div className="p1">
-            <div style={{ width: "160px" }}>Үйлчилгээ</div>
-            {modaldata.SERVICE}
-          </div>
-          <div className="p2">
-            <div style={{ width: "160px" }}>Email : </div>
-            {modaldata.EMAIL}
-          </div>
-          <div style={{ height: "65px" }} className="p1">
-            Нэмэлт тайлбар: {modaldata.ADDITIONAL}
-          </div>
           <p>
             <>
-              {modaldata.OPERATOR_STATUS === null ? (
+              {modaldata.ID_CHECK === 0 ? (
                 <Checkbox
                   onChange={onChange}
                   checked={checkValue === modaldata.ID ? checkState : false}
                   value={modaldata.ID}
                 >
                   {checkState === true && checkValue === modaldata.ID
-                    ? "Шивэгдсэн"
-                    : "Шивэгдээгүй"}
+                    ? "Баталгаажсан"
+                    : "Баталгаажгүй"}
                 </Checkbox>
               ) : (
-                <Checkbox checked={true}>Шивэгдсэн</Checkbox>
+                <Checkbox checked={true}>Баталгаажсан</Checkbox>
               )}
             </>
           </p>
@@ -390,47 +356,33 @@ function EbarimtId() {
             }}
           />
         </Space>
-        {/* <Input
-                        onChange={e => {
-                          if(e.target.value.length > 0 ){
-                            const filteredData = data.filter(entry =>
-                              entry.MOBILE.toLowerCase().includes(e.target.value)
-                            );
-                            setData(filteredData);
-                          
-                          } else {
-                            console.log(e.target.value.length)
-                            window.location.reload(false);
-                          }                    
-                                      }}
-              placeholder=" Утасны дугаараар хайна уу"
-              style={{marginBottom: "20px"}}
-              suffix={<SearchOutlined />}
-            /> */}
-
         <Select
           style={{
-            width: 200,
+            width: 230,
             margin: "0 5px 0 5px",
           }}
           placeholder="Хайлт төрөл"
           onChange={handlerBtnTwo}
           options={[
             {
+              value: "ebarimt_id",
+              label: "Ибаримт дугаар",
+            },
+            {
               value: "mobile",
               label: "Утасны дугаар",
             },
             {
-              value: "register",
+              value: "regno",
               label: "Регистерийн дугаар",
             },
             {
-              value: "city",
-              label: "Хаяг",
+              value: "cust_id",
+              label: "Гэрээний дугаар",
             },
           ]}
         />
-        <Search placeholder="Утасны дугаарыг оруулна уу" onSearch={onSearch} />
+        <Search placeholder="Хайлт" onSearch={onSearch} />
       </div>
       <Table dataSource={data} columns={columns} pagination={false} />
 
